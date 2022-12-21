9a4de0d8ca82581093fcf3eb870e80b43a5cadf7
tangyiyi <1040246487@qq.com>
2022-12-21 20:30:21 +0800

    使用bulma完成首页基础布局

---
+++ src/layout/Layout.vue
@@ -0,0 +1,79 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+<template>
+  <div>
+    <header class="header">
+      <nav
+        class="navbar is-spaced has-shadow"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div class="container">
+          <div class="navbar-brand">
+            <a class="navbar-item" href="https://bulma.zcopy.site">
+              <img
+                src="https://bulma.zcopy.site/images/bulma-logo.png"
+                width="112"
+                height="28"
+              />
+            </a>
+
+            <a
+              role="button"
+              class="navbar-burger"
+              aria-label="menu"
+              aria-expanded="false"
+              data-target="navbarBasicExample"
+            >
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <a-space>
+                  <a-button type="primary">Login</a-button>
+                  <a-button>Register</a-button>
+                </a-space>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <div class="container">
+      <video class="container" width="800" height="800" controls autoplay>
+        <source
+          src=" https://www.runoob.com/try/demo_source/movie.ogg"
+          type="video/ogg"
+        />
+        <source
+          src=" https://www.runoob.com/try/demo_source/movie.ogg"
+          type="video/mp4"
+        />
+        <source
+          src=" https://www.runoob.com/try/demo_source/movie.ogg"
+          type="video/webm"
+        />
+        <object
+          data=" https://www.runoob.com/try/demo_source/movie.ogg"
+          width="320"
+          height="240"
+        >
+          <embed width="320" height="240" src="movie.swf" />
+        </object>
+      </video>
+    </div>
+    <footer class="">底部</footer>
+  </div>
+  <!-- <RouterView /> -->
+</template>
+
+<style lang="scss" scoped>
+@import "bulma/sass/utilities/_all.sass";
+@import "bulma/sass/components/navbar.sass";
+@import "bulma/sass/elements/container.sass";
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,29 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../components/Login.vue'
-import HomeView from '../views/HomeView.vue'
-import TheHall from '../components/TheHall.vue'
+import { createRouter, createWebHistory } from "vue-router";
+// import Login from "../components/Login.vue";
+import HomeView from "../views/HomeView.vue";
+import TheHall from "../components/TheHall.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'login',
-      component: Login
+      path: "/",
+      name: "layout",
+      component: () => import("../layout/Layout.vue"),
+      redirect: "/home",
+      children: [
+        {
+          path: "/home",
+          name: "home",
+          component: HomeView,
+        },
+        {
+          path: "/theHall",
+          name: "theHall",
+          component: TheHall,
+        },
+      ],
     },
-    {
-      path: '/home',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/theHall',
-      name: 'theHall',
-      component: TheHall
-    },
-  ]
-})
+  ],
+});
 
-export default router
+export default router;
